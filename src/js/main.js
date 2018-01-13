4b4907ac6cd9ea332b935e0d00ab16acd2a5265e
Nicolai Schmid <nicolai@schmid.uno>
2018-01-13 01:16:39 +0100

    Fix: Improved the a11y by using correct headings and defining the right aria attributes

--- src/js/main.js
+++ src/js/main.js
@@ -147,7 +147,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = `Picture of ${restaurant.name} Restaurant`;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
